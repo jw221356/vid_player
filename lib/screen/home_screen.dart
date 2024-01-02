54991c5f8de96c6f2dd49f108ca63602bcb1e58f
jeong won <jw221356@naver.com>
2024-01-02 16:25:26 +0900

    화면 UI 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
     );
+  }
+  Widget renderEmpty(){
+    return Container();
+  }
+
+  Widget renderVideo(){
+    return Container();
   }
 }
