73f730a7d0564cdb19364f2e440fdf526306a03b
jeong won <jw221356@naver.com>
2024-01-02 18:50:48 +0900

    컨트롤러 감추기 기능

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -6,9 +6,7 @@ import 'package:vid_player/component/custom_icon_button.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:io';
 
-
 class CustomVideoPlayer extends StatefulWidget {
-
   final XFile video;
   final GestureTapCallback onNewVideoPressed;
 
@@ -24,20 +22,19 @@ class CustomVideoPlayer extends StatefulWidget {
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoPlayerController;
+  bool showControls = false;
 
   @override
-  // covariant 키워드는 CustomVideoPlayer 클래스의 상속된 값도 허가해줍니다.
   void didUpdateWidget(covariant CustomVideoPlayer oldWidget) {
     super.didUpdateWidget(oldWidget);
 
-    // ➊ 새로 선택한 동영상이 같은 동영상인지 확인
     if (oldWidget.video.path != widget.video.path) {
       initializeController();
     }
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     initializeController();
@@ -49,7 +46,7 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     );
 
     await videoController.initialize();
-    
+
     videoController.addListener(videoControllerListener);
 
     setState(() {
@@ -63,7 +60,6 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   void dispose() {
-    // ➋ listener 삭제
     videoController?.removeListener(videoControllerListener);
     super.dispose();
   }
@@ -76,71 +72,81 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
       );
     }
 
-    return AspectRatio(
-      aspectRatio: videoController!.value.aspectRatio,
-      child: Stack(
-        children: [
-          VideoPlayer(
-            videoController!,
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Slider(
-              onChanged: (double val){
-                videoController!.seekTo(
-                  Duration(seconds: val.toInt()),
-                );
-              },
-
-              value: videoController!.value.position.inSeconds.toDouble(),
-              min: 0,
-              max: videoController!.value.duration.inSeconds.toDouble(),
-            ),
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: CustomIconButton(
-              onPressed:widget.onNewVideoPressed,
-              iconData: Icons.photo_camera_back,
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CustomIconButton(
-                  onPressed: onReversePressed,
-                  iconData: Icons.rotate_left,
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            showControls = !showControls;
+          });
+        },
+        child: AspectRatio(
+          aspectRatio: videoController!.value.aspectRatio,
+          child: Stack(
+            children: [
+              VideoPlayer(
+                videoController!,
+              ),
+              if (showControls)
+                Container(
+                  color: Colors.black.withOpacity(0.5),
                 ),
-                CustomIconButton(
-                  onPressed: onPlayPressed,
-                  iconData: videoController!.value.isPlaying?
-                      Icons.pause : Icons.play_arrow,
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Slider(
+                  onChanged: (double val) {
+                    videoController!.seekTo(
+                      Duration(seconds: val.toInt()),
+                    );
+                  },
+                  value: videoController!.value.position.inSeconds.toDouble(),
+                  min: 0,
+                  max: videoController!.value.duration.inSeconds.toDouble(),
                 ),
-                CustomIconButton(
-                  onPressed: onForwardPressed,
-                  iconData: Icons.rotate_right,
+              ),
+              if (showControls)
+                Align(
+                  alignment: Alignment.topRight,
+                  child: CustomIconButton(
+                    onPressed: widget.onNewVideoPressed,
+                    iconData: Icons.photo_camera_back,
+                  ),
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-
+              if (showControls)
+                Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CustomIconButton(
+                        onPressed: onReversePressed,
+                        iconData: Icons.rotate_left,
+                      ),
+                      CustomIconButton(
+                        onPressed: onPlayPressed,
+                        iconData: videoController!.value.isPlaying
+                            ? Icons.pause
+                            : Icons.play_arrow,
+                      ),
+                      CustomIconButton(
+                        onPressed: onForwardPressed,
+                        iconData: Icons.rotate_right,
+                      ),
+                    ],
+                  ),
+                )
+            ],
+          ),
+        )
     );
   }
-  
+
   void onForwardPressed() {
-    // ➋ 앞으로 감기 버튼 눌렀을 때 실행할 함수
-    final maxPosition = videoController!.value.duration; // 동영상 길이
+    final maxPosition = videoController!.value.duration;
     final currentPosition = videoController!.value.position;
 
-    Duration position = maxPosition; // 동영상 길이로 실행 위치 초기화
+    Duration position = maxPosition;
 
-    // 동영상 길이에서 3초를 뺀 값보다 현재 위치가 짧을 때만 3초 더하기
     if ((maxPosition - Duration(seconds: 3)).inSeconds >
         currentPosition.inSeconds) {
       position = currentPosition + Duration(seconds: 3);
@@ -150,7 +156,6 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   }
 
   void onPlayPressed() {
-    // ➌ 재생 버튼을 눌렀을 때 실행할 함수
     if (videoController!.value.isPlaying) {
       videoController!.pause();
     } else {
