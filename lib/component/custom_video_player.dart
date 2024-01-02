795a0813cd6c73e3ef409fd79ec56f9de6524e13
jeong won <jw221356@naver.com>
2024-01-02 20:53:35 +0900

    fix

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,10 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:vid_player/component/custom_icon_button.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:io';
+import 'package:vid_player/component/custom_icon_button.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
@@ -21,7 +19,7 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
-  VideoPlayerController? videoPlayerController;
+  VideoPlayerController? videoController;
   bool showControls = false;
 
   @override
@@ -50,7 +48,7 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     videoController.addListener(videoControllerListener);
 
     setState(() {
-      this.videoPlayerController = videoController;
+      this.videoController = videoController;
     });
   }
 
@@ -67,105 +65,116 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   @override
   Widget build(BuildContext context) {
     if (videoController == null) {
-      return Center(
+      return const Center(
         child: CircularProgressIndicator(),
       );
     }
 
     return GestureDetector(
-        onTap: () {
-          setState(() {
-            showControls = !showControls;
-          });
-        },
-        child: AspectRatio(
-          aspectRatio: videoController!.value.aspectRatio,
-          child: Stack(
-            children: [
-              VideoPlayer(
-                videoController!,
+      onTap: () {
+        setState(() {
+          showControls = !showControls;
+        });
+      },
+      child: AspectRatio(
+        aspectRatio: videoController!.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(
+              videoController!,
+            ),
+            if(showControls)
+              Container(
+                color: Colors.black.withOpacity(0.5),
               ),
-              if (showControls)
-                Container(
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    children: [
-                      renderTimeTextFromDuration(
-                        videoController!.value.position,
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  children: [
+                    renderTimeTextFromDuration(
+                      videoController!.value.position,
+                    ),
+                    Expanded(
+                      child: Slider(
+                        onChanged: (double val) {
+                          videoController!.seekTo(
+                            Duration(seconds: val.toInt()),
+                          );
+                        },
+                        value: videoController!.value.position.inSeconds
+                            .toDouble(),
+                        min: 0,
+                        max: videoController!.value.duration.inSeconds
+                            .toDouble(),
                       ),
-                      Expanded(
-                        child: Slider(
-                          onChanged: (double val) {
-                            videoController!.seekTo(
-                              Duration(seconds: val.toInt()),
-                            );
-                          },
-                          value: videoController!.value.position.inSeconds
-                              .toDouble(),
-                          min: 0,
-                          max: videoController!.value.duration.inSeconds
-                              .toDouble(),
-                        ),
-                      ),
-                      renderTimeTextFromDuration(
-
-                        videoController!.value.duration,
-                      ),
-                    ],
-                  ),
+                    ),
+                    renderTimeTextFromDuration(
+                      videoController!.value.duration,
+                    ),
+                  ],
                 ),
               ),
-              if (showControls)
-                Align(
-                  alignment: Alignment.topRight,
-                  child: CustomIconButton(
-                    onPressed: widget.onNewVideoPressed,
-                    iconData: Icons.photo_camera_back,
-                  ),
+            ),
+            if(showControls)
+              Align(
+                alignment: Alignment.topRight,
+                child: CustomIconButton(
+                  onPressed: widget.onNewVideoPressed,
+                  iconData: Icons.photo_camera_back,
                 ),
-              if (showControls)
-                Align(
-                  alignment: Alignment.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CustomIconButton(
-                        onPressed: onReversePressed,
-                        iconData: Icons.rotate_left,
-                      ),
-                      CustomIconButton(
-                        onPressed: onPlayPressed,
-                        iconData: videoController!.value.isPlaying
-                            ? Icons.pause
-                            : Icons.play_arrow,
-                      ),
-                      CustomIconButton(
-                        onPressed: onForwardPressed,
-                        iconData: Icons.rotate_right,
-                      ),
-                    ],
-                  ),
-                )
-            ],
-          ),
-        )
+              ),
+            if (showControls)
+              Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CustomIconButton(
+                      onPressed: onReversePressed,
+                      iconData: Icons.rotate_left,
+                    ),
+                    CustomIconButton(
+                      onPressed: onPlayPressed,
+                      iconData: videoController!.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow,
+                    ),
+                    CustomIconButton(
+                      onPressed: onForwardPressed,
+                      iconData: Icons.rotate_right,
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 
   Widget renderTimeTextFromDuration(Duration duration) {
     return Text(
       '${duration.inMinutes.toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}',
-      style: TextStyle(
+      style: const TextStyle(
         color: Colors.white,
       ),
     );
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+
+    Duration position = const Duration();
+
+    if (currentPosition.inSeconds > 3) {
+      position = currentPosition - const Duration(seconds: 3);
+    }
+
+    videoController!.seekTo(position);
   }
 
   void onForwardPressed() {
@@ -174,9 +183,9 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
     Duration position = maxPosition;
 
-    if ((maxPosition - Duration(seconds: 3)).inSeconds >
+    if ((maxPosition - const Duration(seconds: 3)).inSeconds >
         currentPosition.inSeconds) {
-      position = currentPosition + Duration(seconds: 3);
+      position = currentPosition + const Duration(seconds: 3);
     }
 
     videoController!.seekTo(position);
